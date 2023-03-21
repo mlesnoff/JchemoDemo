65e120a8eb760fc0933d793f9b6d4e9f23636ef4
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2023-03-21 20:41:01 +0100

    Create plskern_benchmark.jl

---
+++ src/Examples_Jchemo/Regression/plskern_benchmark.jl
@@ -0,0 +1,15 @@
+using Jchemo
+
+n = 2000 ; p = 1000 
+#n = 5000 ; p = 1000 
+#n = 50000 ; p = 1000 
+#n = 500000 ; p = 500 
+#n = 10^6 ; p = 500 
+q = 10 
+X = rand(n, p)
+Y = rand(n, q) 
+y = Y[:, 1]
+w = rand(n) 
+nlv = 25 
+@time plskern(X, y; nlv = nlv) ;
+@time plskern(X, Y; nlv = nlv) ;
