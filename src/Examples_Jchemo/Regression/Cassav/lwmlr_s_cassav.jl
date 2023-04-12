a435ba6c1925df1d3b3bd98298257888473884d4
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2023-04-12 18:54:43 +0200

    Update lwmlr_s_cassav.jl

--- src/Examples_Jchemo/Regression/Cassav/lwmlr_s_cassav.jl
+++ src/Examples_Jchemo/Regression/Cassav/lwmlr_s_cassav.jl
@@ -1,10 +1,9 @@
 using JLD2, CairoMakie, StatsBase
 using Jchemo, JchemoData
-using Loess
 
 using JchemoData, JLD2, CairoMakie
 path_jdat = dirname(dirname(pathof(JchemoData)))
-db = joinpath(path_jdat, "data", "cassav.jld2")
+db = joinpath(path_jdat, "data/cassav.jld2")
 @load db dat
 pnames(dat)
 
