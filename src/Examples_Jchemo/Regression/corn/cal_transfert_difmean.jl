0c89be84d2e3a899305b071ad68c890d0b5bc378
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2023-05-24 15:57:04 +0200

    Update cal_transfert_difmean.jl

--- src/Examples_Jchemo/Regression/corn/cal_transfert_difmean.jl
+++ src/Examples_Jchemo/Regression/corn/cal_transfert_difmean.jl
@@ -10,76 +10,127 @@ pnames(dat)
 Xm5 = dat.Xm5
 Xmp5 = dat.Xmp5
 Xmp6 = dat.Xmp6
-Xm5nbs = dat.Xm5nbs
-Xmp5nbs = dat.Xmp5nbs
-Xmp6nbs = dat.Xmp6nbs
 Y = dat.Y 
 ntot = nro(Xm5)
 wl = names(dat.Xm5)
 wl_num = parse.(Float64, wl)
+namy = names(Y)
 
 summ(Y).res
 
-plotsp(Xm5, wl_num).f
+plotsp(Xm5, wl_num;
+    xlabel = "Wavelength (nm)", ylabel = "Reflectance").f
 
 fpreproc(X) = savgol(snv(X); f = 11, d = 2, pol = 3) 
 Xpm5 = fpreproc(Xm5)
 Xpmp5 = fpreproc(Xmp5)
 Xpmp6 = fpreproc(Xmp6)
 
-
-
-
+## Data selection (machines 1 and 2) for the example
+X1 = copy(Xpm5)
+X2 = copy(Xpmp6)    
 m = ntot / 2
 res = sampdp(hcat(X1, X2); k = m) ;
 pnames(res)
 res.train
 res.test
-res.remain
-s = vcat(res.train, res.remain)
+s = res.train
+## Machine 1
 X1train = X1[s, :]
 Y1train = Y[s, :]
 X1test = rmrow(X1, s)
 Y1test = rmrow(Y, s)
-##
+## Machine 2
 X2test = X2[s, :]
 Y2test = Y[s, :]
 X2train = rmrow(X2, s)
 Y2train = rmrow(Y, s)
-##
+## End
 n1train = nro(X1train)
 n1test = nro(X1test)
 n2train = nro(X2train)
 n2test = nro(X2test)
 (n1 = n1, n2, n1train, n1test, n2train, n2test)
 
-summ(Y).res
-j = 1
+## Selection of the y-variable
+j = 1  
 println(namy[j])
 y1train = Y1train[:, j]
 y1test = Y1test[:, j]
 y2train = Y2train[:, j]
 y2test = Y2test[:, j]
 
-zX = copy(X1train) ; zy = copy(y1train) ; zn = copy(n1train) ; zXtest = copy(X1test) ; zytest = copy(y1test) 
-#zX = copy(X2train) ; zy = copy(y2train) ; zn = copy(n2train) ; zXtest = copy(X2test) ; zytest = copy(y2test) 
+#### Model tuning and performance on the reference data (for a given machine)
+## On machine 1
+zXtrain = copy(X1train) ; zytrain = copy(y1train) ; zn = copy(n1train) ; zXtest = copy(X1test) ; zytest = copy(y1test) 
+## On machine 2
+#zXtrain = copy(X2train) ; zytrain = copy(y2train) ; zn = copy(n2train) ; zXtest = copy(X2test) ; zytest = copy(y2test) 
 K = 5
 segm = segmkf(zn, K; rep = 20)
 nlv = 0:15
-res = gridcvlv(zX, zy; segm = segm,
+res = gridcvlv(zXtrain, zytrain; segm = segm,
     score = rmsep, fun = plskern, nlv = nlv).res
 plotgrid(res.nlv, res.y1; step = 1).f
 u = findall(res.y1 .== minimum(res.y1))[1]
 res[u, :]
-fm = plskern(zX, zy; nlv = res.nlv[u]) ;
+fm = plskern(zXtrain, zytrain; nlv = res.nlv[u]) ;
 pred = Jchemo.predict(fm, zXtest).pred
 println(rmsep(pred, zytest))
 plotxy(vec(pred), zytest;
     bisect = true, xlabel = "Prediction",
     ylabel = "Observed").f
 
-nlv0 = 10 ; rep = 1000
-D = Jchemo.mcdif(X1train, X2train; rep = rep).D
+#### Model tuning after transfert (for a given machine)
+D = Jchemo.difmean(X1train, X2train).D
+M = eposvd(D; nlv = 1).M
+zXtrain = X1train * M ; zytrain = copy(y1train) ; zn = copy(n1train) 
+#zXtrain = X2train * M ; zytrain = copy(y2train) ; zn = copy(n2train) 
+K = 5
+segm = segmkf(zn, K; rep = 20)
+nlv = 0:15
+res = gridcvlv(zXtrain, zytrain; segm = segm,
+    score = rmsep, fun = plskern, nlv = nlv).res
+plotgrid(res.nlv, res.y1).f
+u = findall(res.y1 .== minimum(res.y1))[1]
+res[u, :]
+
+#### Model performance after transfert 
+res = difmean(X1train, X2train)
+fm0 = eposvd(res.D; nlv = nlv0) ;
+## On X1
+zXtrain = X1train * fm0.M
+zytrain = copy(y1train)
+zXtest = copy(X1test)
+#zXtest = X1test * fm0.M    # not required if PLSR (othogonalization is embedded) 
+zytest = copy(y1test)
+## On X2
+#zXtrain = X2train * fm0.M
+#zytrain = copy(y2train)
+#zXtest = copy(X2test)
+#zXtest = X1test * fm0.M    # not required if PLSR (othogonalization is embedded) 
+#zytest = copy(y2test)
+## End
+nlv = 7   # depends on X1 vs X2 and y-variable
+fm = plskern(zXtrain, zytrain; nlv = nlv) ;
+pred = Jchemo.predict(fm, zXtest).pred
+println(rmsep(pred, zytest))
+plotxy(vec(pred), zytest;
+    bisect = true, xlabel = "Prediction",
+    ylabel = "Observed").f
+
+
+
+
+
+
+fm = plskern(zXtrain, zytrain; nlv = res.nlv[u]) ;
+pred = Jchemo.predict(fm, zXtest).pred
+println(rmsep(pred, zytest))
+plotxy(vec(pred), zytest;
+    bisect = true, xlabel = "Prediction",
+    ylabel = "Observed").f
+
+
 scor = nothing
 for a = 1:nlv0
     D = Jchemo.difload(X1train, X2train; nlv = nlv0).D
@@ -134,32 +185,23 @@ D' * inv(D * D') * D
 
 
 
-## High: good example but only 1 nlv (difmean is enough,
-## but mcdif, difload works good)  
-## Improve the tuning: separate X2 to train+test (X2train
-## used to compute D; X2test should never be used in the calibration)
 
-sourcedir(path_fun)
-nlv0 = 1 ; nlv = 27   # Mid
-#nlv0 = 1 ; nlv = 27   # High
-res = Jchemo.difmean(X1, X2train)
-fm0 = eposvd(res.D; nlv = nlv0) ;
-X1_transf = X1 * fm0.M
-zX2 = copy(X2test)
-#zX2 = X2test * fm0.M    # not required if PLSR (othogonalization is embedded) 
-fm = plskern(X1_transf, y1; nlv = nlv) ;
-pred = Jchemo.predict(fm, zX2).pred
-println(rmsep(pred, y2test))
-plotxy(vec(pred), y2test;
-    bisect = true, xlabel = "Prediction",
-    ylabel = "Observed").f
 
-sourcedir(path_fun)
-z1 = rand(2, 10)
-z2 = 3 * z1
-res = Jchemo.difmean(z1, z2)
-res.D
+
+X1 = copy(Xpm5) ; X2 = copy(Xpmp6)
+res = Jchemo.difmean(X1, X2) ;
 fm0 = eposvd(res.D; nlv = 1) ;
-fm0.P
-z1 * fm0.M
-z2 * fm0.M
+X1c = X1 * fm0.M
+X2c = X2 * fm0.M
+zX1 = copy(X1) ; zX2 = copy(X2)
+#zX1 = copy(X1c) ; zX2 = copy(X2c)
+fm = pcasvd(zX1; nlv = 10) ;
+T1 = fm.T
+T2 = Jchemo.transform(fm, zX2)
+i = 1
+f, ax = plotxy(T1[:, i:(i + 1)];
+    xlabel = string("PC", i), ylabel = string("PC", i + 1))
+scatter!(ax, T2[:, i:(i + 1)])
+f
+
+
