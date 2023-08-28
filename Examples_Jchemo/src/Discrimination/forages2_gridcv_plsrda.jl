1cb7bf687fd14fe16a9741722e6dd6d45338ba0f
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2023-08-28 11:47:40 +0200

    Update forages2_gridcv_plsrda.jl

--- Examples_Jchemo/src/Discrimination/forages2_gridcv_plsrda.jl
+++ Examples_Jchemo/src/Discrimination/forages2_gridcv_plsrda.jl
@@ -44,34 +44,53 @@ ntrain = nro(Xtrain)
 ntest = nro(Xtest)
 (ntot = ntot, ntrain, ntest)
 
+#-
 K = 3 ; segm = segmkf(ntrain, K; rep = 10)         # K-fold CV   
 #m = 100 ; segm = segmts(ntrain, m; rep = 30)      # Test-set CV
 
+#-
 nlv = 0:50
 res = gridcvlv(Xtrain, ytrain; segm = segm, 
     score = err, fun = plsrda, nlv = nlv, verbose = false).res
+
+#-
 u = findall(res.y1 .== minimum(res.y1))[1] 
 res[u, :]
+
+#-
 plotgrid(res.nlv, res.y1; step = 5,
     xlabel = "Nb. LVs", ylabel = "ERR").f
 
+#-
 fm = plsrda(Xtrain, ytrain; nlv = res.nlv[u]) ;
 pred = Jchemo.predict(fm, Xtest).pred
 err(pred, ytest)
 
+#-
 cf = confusion(pred, ytest) ;
 cf.cnt
+
+#-
 cf.pct
+
+#-
 plotconf(cf).f
 
+#-
 ## PLSLDA
 nlv = 1:50  ## !!: Does not start from nlv=0 (since LDA on scores)
 res = gridcvlv(Xtrain, ytrain; segm = segm, 
     score = err, fun = plslda, nlv = nlv, verbose = false).res
+
+#-
 u = findall(res.y1 .== minimum(res.y1))[1] 
 res[u, :]
+
+#-
 plotgrid(res.nlv, res.y1; step = 5,
     xlabel = "Nb. LVs", ylabel = "ERR").f
+
+#-
 fm = plslda(Xtrain, ytrain; nlv = res.nlv[u]) ;
 pred = Jchemo.predict(fm, Xtest).pred
 err(pred, ytest)
