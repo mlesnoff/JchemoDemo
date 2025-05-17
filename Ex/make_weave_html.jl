79de3447f7bf6dd4796240f87e495909c6920ffa
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2025-05-17 10:13:13 +0200

    new

--- Ex/make_weave_html.jl
+++ Ex/make_weave_html.jl
@@ -1,11 +1,30 @@
 using Weave
 
+## Global paths
 path = "C:/Users/lesnoff/.julia/dev"
 path_jmd = joinpath(path, "JchemoDemo/Ex/jmd")
 path_html = joinpath(path, "JchemoDemo/docs/src/assets/html")
+## End
+
+
+nam = "Exploratory/Pca"
+zpath = joinpath(path_jmd, nam)
+@show f = filter(endswith("jmd"), readdir(zpath))
+nf = length(f) 
+for i = 1:nf 
+    @show string("-------- ", i)
+    @show f[i]
+    #i = 11
+    f[i]
+    db = joinpath(zpath, f[i])
+    out_path = joinpath(path_html, nam)
+    println("Time")
+    @time weave(db; out_path = out_path, doctype = "md2html", fig_path = nothing, fig_ext = nothing)
+end
 
 
 
+C:\Users\lesnoff\.julia\dev\JchemoDemo\Ex\jmd\Exploratory\Pca
 
 
 
