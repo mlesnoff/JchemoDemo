74d331e1b985a84862fe9e91fbc75ab45306b97f
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2025-05-20 15:44:06 +0200

    new

--- Ex/make_weave_html.jl
+++ Ex/make_weave_html.jl
@@ -8,55 +8,19 @@ path_html = joinpath(path, "JchemoDemo/docs/src/assets/html")
 
 nam = "Exploratory/Pca"
 #nam = "Regression/Plsr"
+#nam = "Regression/Others"
 path_scripts = joinpath(path_jmd, nam)
 @show f = filter(endswith("jmd"), readdir(path_scripts))
 nf = length(f) 
 for i = 1:nf 
     #i = 4
-    @show string("-------- script:", i)
+    @show string("-------- script: ", i)
     @show f[i]
     f[i]
     nam_script = joinpath(path_scripts, f[i])
     out_path = joinpath(path_html, nam)
     println("Time")
     @time weave(nam_script; out_path, doctype = "md2html", fig_path = nothing, fig_ext = nothing)
-end
-
-
-
-
-
-
-
-
-
-
-
-
-
-nam = "cassav"
-#nam = "challenge2018"
-#nam = "corn"
-#nam = "forages2"
-#nam = "iris"
-#nam = "mnist20pct"
-#nam = "multifruit"
-#nam = "octane"
-#nam = "swissroll"
-#nam = "tecator"
-#nam = "wines_sensory"
-zpath = joinpath(path_jmd, nam)
-@show f = filter(endswith("jmd"), readdir(zpath))
-nf = length(f) 
-for i = 6:6 #1:nf 
-    @show i
-    @show f[i]
-    #i = 11
-    f[i]
-    db = joinpath(zpath, f[i])
-    out_path = joinpath(path_html, nam)
-    println("Time")
-    @time weave(db; out_path = out_path, doctype = "md2html", fig_path = nothing, fig_ext = nothing)
 end
 
 
