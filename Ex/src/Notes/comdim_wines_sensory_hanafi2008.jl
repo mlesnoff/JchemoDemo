68d147a229d65ebcdd075c8bbd3836ad2b58466d
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2025-06-08 15:11:29 +0200

    new

---
+++ Ex/src/Notes/comdim_wines_sensory_hanafi2008.jl
@@ -0,0 +1,116 @@
+
+using Jchemo, JchemoData
+using JLD2, CairoMakie
+
+
+path_jdat = dirname(dirname(pathof(JchemoData)))
+db = joinpath(path_jdat, "data/wines_sensory.jld2")
+@load db dat
+@names dat
+
+
+X1 = dat.X1 
+X2 = dat.X2
+X3 = dat.X3
+X4 = dat.X4
+Xbl = [X1, X2, X3, X4] ;   # Same as: Xbl = [[X1]; [X2]; [X3]; [X4]]
+
+
+n = nro(X1)
+nbl = length(Xbl)
+
+
+nlv = 3
+bscal = :frob
+model = comdim(; nlv, bscal)
+fit!(model, Xbl)
+fitm = model.fitm ;
+@names fitm
+
+
+@head T = fitm.T
+
+
+i = 1 
+f, ax = plotxy(T[:, i], T[:, i + 1]; zeros = true, xlabel = string("PC", i), ylabel = string("PC", i + 1)) 
+text!(ax, T[:, i], T[:, i + 1]; text = string.("w", 1:n), fontsize = 15)
+f
+
+
+Tbl = fitm.Tbl
+Vbl = fitm.Vbl
+
+
+k = 1
+Tbl[k]
+Vbl[k]
+
+
+res = summary(model, Xbl) ;
+@names res
+
+
+res.explvarxx
+
+
+explxbl = res.explxbl  # = specific weights 'lb' when 'bscal = :frob'
+rowsum(explxbl)
+
+
+contrxbl2t = res.contrxbl2t
+colsum(contrxbl2t)
+
+
+z = contrxbl2t
+i = 1
+f, ax = plotxy(z[:, i], z[:, i + 1]; zeros = true, xlabel = string("PC", i), 
+    ylabel = string("PC", i + 1)) 
+text!(ax, z[:, i], z[:, i + 1]; text = string.("j", 1:nbl), fontsize = 15) 
+xlims!(ax, [0, .4]) 
+ylims!(ax, [0, .9]) 
+f
+
+
+res.rvxbl2t
+
+
+corx2t = res.corx2t
+
+
+z = corx2t 
+nvar = nro(z) 
+i = 1
+f, ax = plotxy(z[:, i], z[:, i + 1]; zeros = true, xlabel = string("PC", i), 
+    ylabel = string("PC", i + 1)) 
+text!(ax, z[:, i], z[:, i + 1]; text = string.("v", 1:nvar), fontsize = 15) 
+xlims!(ax, [-1, 1]) 
+ylims!(ax, [-1, 1]) 
+f
+
+
+k = 4
+v = Vbl[k]
+nam = names(Xbl[k])
+i = 1
+f, ax = plotxy(T[:, i], T[:, i + 1]; zeros = true, xlabel = string("PC", i), 
+    ylabel = string("PC", i + 1)) 
+text!(ax, T[:, i], T[:, i + 1]; text = string.("w", 1:n), fontsize = 15) 
+scatter!(ax, v[:, i], v[:, i + 1]) 
+text!(ax, v[:, i], v[:, i + 1]; text = nam, fontsize = 15) 
+ylims!(ax, [-1, 1]) 
+f
+
+
+k = 4  # fourth block
+z = Tbl[k] 
+v = Vbl[k] 
+nam = names(Xbl[k]) 
+i = 1 ;
+f, ax = plotxy(z[:, i], z[:, i + 1]; zeros = true, xlabel = string("PC", i), 
+    ylabel = string("PC", i + 1), title = string("Block ", k)) 
+text!(ax, z[:, i], z[:, i + 1]; text = string.("w", 1:n), fontsize = 15) 
+scatter!(ax, v[:, i], v[:, i + 1]) 
+text!(ax, v[:, i], v[:, i + 1]; text = nam, fontsize = 15) 
+ylims!(ax, [-1, 1]) 
+f
+
--- docs/src/notes.md
+++ docs/src/notes.md
@@ -1,3 +1,4 @@
 ```@raw html
+    <p><a href="../assets/html/Notes/comdim_wines_sensory_hanafi2008.html">ComDim - wines_sensory - Hanafi2008</a></p>
     <p><a href="../assets/html/Notes/knn_lwplsr_challenge2018.html">Introduction to kNN-LWPLSR - challenge2018</a></p>
 ```
